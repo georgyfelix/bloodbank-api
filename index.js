c1e1526d038b2a67cab5b1992b3d92e00eb9f732
Georgy Felix <33150925+georgyfelix@users.noreply.github.com>
2020-01-11 00:23:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,10 +20,13 @@ conn.connect((err) => {
     console.log('Mysql Connected...');
 });
 
-app.listen(3000, () => {
-    console.log('Server started on port 3000...');
-});
 
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+app.listen(server_port, server_ip_address, function () {
+    console.log("Listening on " + server_ip_address + ", port " + server_port)
+});
 
 app.post('/api/RegisterNewUser', (req, res) => {
     let data = { 
