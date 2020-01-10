0263e0fa8684d9e87c82da5cae0d6bb781aacda3
Georgy Felix <33150925+georgyfelix@users.noreply.github.com>
2020-01-11 00:47:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ conn.connect((err) => {
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
-app.listen(server_port, server_ip_address, function () {
+var server = app.listen(server_port, server_ip_address, function () {
     console.log("Listening on " + server_ip_address + ", port " + server_port);
     console.log(process.env.OPENSHIFT_NODEJS_PORT);
     console.log(process.env.OPENSHIFT_NODEJS_IP);
